ac6173a5b08220423698fa9818dee6f6bf631d5b
janjira111 <janjira_thanach@cmu.ac.th>
2025-03-03 02:12:36 +0700

    completed

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -1,61 +1,73 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
+class Node
+{
+public:
+	int data;
+	Node *next;
+	~Node();
 };
 
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
+class List
+{
+public:
+	Node *root;
+	int size;
+	void show();
+	void append(int);
+	void insert(int, int);
+	void remove(int);
 };
 
-Node::~Node(){
-    cout << data << " was deleted.\n";
+Node::~Node()
+{
+	cout << data << " was deleted.\n";
 }
 
-void List::insert(int d,int idx){	
+void List::insert(int d, int idx)
+{
 	Node *n = new Node;
 	n->data = d;
-	
-	if(idx == 0){
+
+	if (idx == 0)
+	{
 		n->next = root;
 		root = n;
 		return;
 	}
 	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
+	for (int i = 0; i < idx - 1; i++)
+	{
 		current = current->next;
 	}
 	n->next = current->next;
-	current->next = n;		
+	current->next = n;
 }
 
-void List::show(){
+void List::show()
+{
 	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
+	cout << current->data << " ";
+	while (current->next)
+	{
 		current = current->next;
 		cout << current->data << " ";
-	}	
+	}
 }
 
-void List::append(int d){	
+void List::append(int d)
+{
 	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
-	else{
+	n->data = d;
+	n->next = NULL;
+	if (root == NULL)
+		root = n;
+	else
+	{
 		Node *current = root;
-		while(current->next){
+		while (current->next)
+		{
 			current = current->next;
 		}
 		current->next = n;
@@ -63,4 +75,26 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+// Write List::remove() here
+
+void List::remove(int idx)
+{
+	size--;
+	Node *current = root;
+	if (idx == 0)
+	{
+		root = root->next;
+		delete current;
+	}
+	else
+	{
+		Node *n = NULL;
+		for (int i = 0; i < idx; i++)
+		{
+			n = current;
+			current = current->next;
+		}
+		n->next = current->next;
+		delete current;
+	}
+}
